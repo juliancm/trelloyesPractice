5114c5352477b1cc068db3313118684edc6e0b64
juliancm <56898577+juliancm@users.noreply.github.com>
2020-04-08 16:06:55 -0500

    Create Card.js

---
+++ Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card (props) {
+  return (
+    <div className="Card">
+      <button type="button">delete</button>
+      <h3>First card</h3>
+      <p>lorem ipsum</p>
+    </div>
+  )
+}
+
+export default Card
